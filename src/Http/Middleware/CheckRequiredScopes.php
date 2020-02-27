c8d99e967b6c35410806c2a3236ddec1ba84a5cf
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-27 10:04:35 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/CheckRequiredScopes.php
+++ src/Http/Middleware/CheckRequiredScopes.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Auth\Http\Middleware;
-
 
 use Closure;
 use Illuminate\Database\Eloquent\Collection;
@@ -39,15 +61,17 @@ class CheckRequiredScopes
     {
         $characters = $this->charactersWithRequiredSsoScopes();
 
-        if($characters->isEmpty())
+        if ($characters->isEmpty()) {
             return $next($request);
+        }
 
         $this->buildRequiredScopes($characters);
 
         $this->buildDifferences();
 
-        if($this->getMissingcharacterscopes()->isNotEmpty())
+        if ($this->getMissingcharacterscopes()->isNotEmpty()) {
             return $this->redirectTo($this->getMissingcharacterscopes());
+        }
 
         return $next($request);
     }
@@ -57,7 +81,6 @@ class CheckRequiredScopes
      */
     public function getMissingcharacterscopes(): \Illuminate\Support\Collection
     {
-
         return $this->missing_character_scopes;
     }
 
@@ -66,11 +89,10 @@ class CheckRequiredScopes
      */
     public function getRequiredScopes(): \Illuminate\Support\Collection
     {
-
         return $this->required_scopes->unique();
     }
 
-    private function charactersWithRequiredSsoScopes() : Collection
+    private function charactersWithRequiredSsoScopes(): Collection
     {
         return auth()->user()->characters()->has('alliance.ssoScopes')
             ->orHas('corporation.ssoScopes')
@@ -82,41 +104,37 @@ class CheckRequiredScopes
             ->get();
     }
 
-
-
     private function buildRequiredScopes(Collection $characters)
     {
         $characters->map(function ($character) {
-
             return [
-                'corporation_scopes' => $character->corporation->ssoScopes->selected_scopes ?? [],
-                'alliance_scopes' => $character->alliance->ssoScopes->selected_scopes ?? [],
+                'corporation_scopes'             => $character->corporation->ssoScopes->selected_scopes ?? [],
+                'alliance_scopes'                => $character->alliance->ssoScopes->selected_scopes ?? [],
                 'application_corporation_scopes' => $character->application->corporation->ssoScopes->selected_scopes ?? [],
-                'application_alliance_scopes' => $character->application->corporation->alliance->ssoScopes->selected_scopes ?? []
+                'application_alliance_scopes'    => $character->application->corporation->alliance->ssoScopes->selected_scopes ?? [],
             ];
         })
         ->flatten(1)
         ->filter()
         ->each(function ($scope_array) {
-
             collect($scope_array)
                 ->flatten()
-                ->map(fn($scope) => explode(',',$scope))
+                ->map(fn ($scope) => explode(',', $scope))
                 ->flatten()
-                ->each(fn($scope) => $this->required_scopes->push($scope));
+                ->each(fn ($scope) => $this->required_scopes->push($scope));
 
-            if (Arr::get($scope_array,'corporation'))
+            if (Arr::get($scope_array, 'corporation')) {
                 $this->required_scopes->push('esi-characters.read_corporation_roles.v1');
+            }
         });
     }
 
     private function buildDifferences()
     {
-
         $this->missing_character_scopes = auth()->user()
             ->characters
-            ->reject(fn($character) => empty(array_diff($this->getRequiredScopes()->toArray(),$character->refresh_token->scopes)))
-            ->map(fn($character) => ['character' => $character, 'missing_scopes' => array_diff($this->getRequiredScopes()->toArray(),$character->refresh_token->scopes)]);
+            ->reject(fn ($character) => empty(array_diff($this->getRequiredScopes()->toArray(), $character->refresh_token->scopes)))
+            ->map(fn ($character)    => ['character' => $character, 'missing_scopes' => array_diff($this->getRequiredScopes()->toArray(), $character->refresh_token->scopes)]);
     }
 
     /*
@@ -126,6 +144,4 @@ class CheckRequiredScopes
     {
         //TODO: extend this with default view.
     }
-
-
 }
