69bb6ac00b7641cd9b2283e5948b89a742352ea8
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-03-13 07:18:05 +0100

    remove passport

--- src/Models/User.php
+++ src/Models/User.php
@@ -30,7 +30,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
 use Seatplus\Auth\Actions\GetAffiliatedCharactersIdsByPermissionArray;
 use Seatplus\Eveapi\Models\Character\CharacterInfo;
 use Spatie\Permission\Traits\HasRoles;
@@ -38,7 +37,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasRoles;
-    use HasApiTokens;
+
     /**
      * The primary key associated with the table.
      *
