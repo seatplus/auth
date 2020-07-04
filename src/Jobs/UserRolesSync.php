be88bcf43f639a4739deea346995a37bee57472e
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-07-04 12:12:48 +0000

    Apply fixes from StyleCI

--- src/Jobs/UserRolesSync.php
+++ src/Jobs/UserRolesSync.php
@@ -1,5 +1,28 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Auth\Jobs;
 
@@ -45,7 +68,7 @@ class UserRolesSync implements ShouldQueue
             ->setDuration(3600);
 
         return [
-            $rate_limited_middleare
+            $rate_limited_middleare,
         ];
     }
 
@@ -54,11 +77,11 @@ class UserRolesSync implements ShouldQueue
 
         $this->user = $user;
         $this->character_ids = User::has('characters.refresh_token')
-            ->with(['characters.refresh_token' => fn($query) => $query->select('character_id')])
+            ->with(['characters.refresh_token' => fn ($query) => $query->select('character_id')])
             ->whereId($this->user->id)
             ->get()
-            ->whenNotEmpty(function($collection) {
-                return $collection->first()->characters->map(fn($character) => $character->refresh_token->character_id);
+            ->whenNotEmpty(function ($collection) {
+                return $collection->first()->characters->map(fn ($character) => $character->refresh_token->character_id);
             })
             ->toArray();
     }
@@ -76,7 +99,7 @@ class UserRolesSync implements ShouldQueue
         return [
             'Roles sync',
             sprintf('user_id: %s', $this->user->id),
-            sprintf('main_character: %s', $this->user->main_character->name ?? '')
+            sprintf('main_character: %s', $this->user->main_character->name ?? ''),
         ];
     }
 
@@ -106,13 +129,12 @@ class UserRolesSync implements ShouldQueue
         $roles = Role::has('acl_affiliations')
             ->with('acl_affiliations.affiliatable.characters')
             ->whereNotIn('type', ['manual', 'automatic'])
-            ->whereHas('acl_members', fn(Builder $query) => $query->where('user_id', $this->user->getAuthIdentifier())
-                ->whereIn('status',['member', 'paused'])
+            ->whereHas('acl_members', fn (Builder $query) => $query->where('user_id', $this->user->getAuthIdentifier())
+                ->whereIn('status', ['member', 'paused'])
             )
             ->cursor();
 
         $this->handleMemberships($roles);
-
 
     }
 
@@ -126,5 +148,4 @@ class UserRolesSync implements ShouldQueue
                 : $role->pauseMember($this->user);
         }
     }
-
 }
