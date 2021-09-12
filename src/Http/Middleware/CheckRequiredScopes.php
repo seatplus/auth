f9751bce82159dcda9d37a0fe7c4300968c51819
Herpaderp Aldent <herpaderpaldent@users.noreply.github.com>
2021-09-12 19:47:22 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/CheckRequiredScopes.php
+++ src/Http/Middleware/CheckRequiredScopes.php
@@ -42,9 +42,8 @@ class CheckRequiredScopes
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
