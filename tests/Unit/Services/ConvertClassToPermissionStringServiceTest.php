cbc06bf5d534a02fe2ddb4cff29f7c1c6a6f43c8
herpaderpaldent <herpaderpaldent@users.noreply.github.com>
2022-06-23 19:22:42 +0000

    Fix styling

--- tests/Unit/Services/ConvertClassToPermissionStringServiceTest.php
+++ tests/Unit/Services/ConvertClassToPermissionStringServiceTest.php
@@ -3,9 +3,6 @@
 use Seatplus\Auth\Services\ConvertClassToPermissionStringService;
 use Seatplus\Eveapi\Models\Assets\Asset;
 
-
 it('converts class to permission string', function () {
-
     expect(ConvertClassToPermissionStringService::get(Asset::class))->toBe('assets');
-
 });
