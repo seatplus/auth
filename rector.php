e310707b2382887c36f237db452f32563a6280d9
Felix Huber <felix.a.huber@gmx.net>
2023-03-22 08:24:44 +0100

    upgrade rector

--- rector.php
+++ rector.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Laravel\Set\LaravelSetList;
 use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     // here we can define, what sets of rules will be applied
     // tip: use "SetList" class to autocomplete sets
     $rectorConfig->sets([
         //SetList::CODE_QUALITY,
-        LaravelSetList::LARAVEL_90,
+        LaravelSetList::LARAVEL_100,
         LaravelSetList::LARAVEL_CODE_QUALITY
     ]);
     // paths to refactor; solid alternative to CLI arguments
