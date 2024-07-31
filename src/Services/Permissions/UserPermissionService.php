dc15d975ab28e7d2341359e5919ea6607cb2ce79
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-07-31 20:00:14 +0200

    feat: add tests for UserPermissionService and implement UserPermissionService class

---
+++ src/Services/Permissions/UserPermissionService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Seatplus\Auth\Services\Permissions;
+
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Models\User;
+use Seatplus\Eveapi\Models\Character\CharacterInfo;
+
+class UserPermissionService
+{
+    private array $corporation_roles = [];
+    private array $permissions = [];
+    private array $character_ids = [];
+    public function __construct(
+        private ?RolePermissionObjectService $role_permission_object_service = null
+    )
+    {
+        $this->role_permission_object_service = $role_permission_object_service ?? new RolePermissionObjectService();
+    }
+
+
+    public function get(User $user): array
+    {
+
+        $user = $user->loadMissing(['characters.roles', 'roles.permissions']);
+
+        $this->buildCorporationRoles($user);
+        $this->buildPermissions($user);
+        $this->buildCharacterIds($user);
+
+
+        return [
+            'corporation_roles' => $this->corporation_roles,
+            'permissions' => $this->permissions,
+            'character_ids' => $this->character_ids,
+            'owned_character_ids' => $user->characters->pluck('character_id')->toArray()
+        ];
+
+    }
+
+    private function buildCorporationRoles(User $user): void
+    {
+        $user
+            ->characters
+            ->each(function (CharacterInfo $character) {
+                foreach ($character->roles->roles ?? [] as $role) {
+                    $this->corporation_roles[$role] = array_merge($this->corporation_roles[$role] ?? [], [$character->corporation_id]);
+                }
+            });
+    }
+
+    private function buildPermissions(User $user): void
+    {
+        $user->roles->each(function (Role $role) {
+            $role_permissions = $this->role_permission_object_service->get($role);
+
+            dump($role_permissions);
+
+            // merge on permissions. The key might exist, so we extend the array
+            $this->permissions = $role_permissions
+                ->mergeRecursive($this->permissions)
+                ->toArray();
+
+        });
+    }
+
+    private function buildCharacterIds(User $user): void
+    {
+        $this->character_ids = $user->characters->pluck('character_id')->toArray();
+    }
+
+
+}
---
+++ tests/Unit/Services/Permissions/UserPermissionServiceTest.php
@@ -0,0 +1,99 @@
+<?php
+
+use Seatplus\Auth\Models\Permissions\Permission;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Services\Permissions\RolePermissionObjectService;
+use Seatplus\Auth\Services\Permissions\UserPermissionService;
+use Seatplus\Eveapi\Models\Character\CharacterRole;
+
+it('builds owned_character_ids from user', function () {
+
+    // Arrange
+    $user = test()->test_user;
+
+    // Act
+    $user_permission_service = new UserPermissionService;
+
+    $result = $user_permission_service->get($user);
+
+    // Assert
+    expect($result['owned_character_ids'])->toBe($user->characters->pluck('character_id')->toArray());
+});
+
+it('builds corporation_roles from user', function () {
+
+    // Arrange
+    $user = test()->test_user;
+
+    CharacterRole::factory()->create([
+        'character_id' => test()->test_character->character_id,
+        'roles' => ['Director', 'Personnel Manager'],
+    ]);
+
+    // Act
+    $user_permission_service = new UserPermissionService;
+
+    $result = $user_permission_service->get($user);
+
+    // Assert
+    expect($result['corporation_roles'])
+        ->toHaveCount(2)
+        ->toHaveKey('Director')
+        ->toHaveKey('Personnel Manager')
+        ->and($result['corporation_roles']['Director'])->toContain(test()->test_character->corporation_id)
+        ->and($result['corporation_roles']['Personnel Manager'])->toContain(test()->test_character->corporation_id);
+});
+
+it('builds permissions from user', function () {
+
+    // Arrange
+    $user = test()->test_user;
+
+    $role1 = Role::create(['name' => Str::random()]);
+    $role2 = Role::create(['name' => Str::random()]);
+
+    // create 3 permissions
+    $permissions = collect([
+        Permission::create(['name' => Str::random()]),
+        Permission::create(['name' => Str::random()]),
+        Permission::create(['name' => Str::random()]),
+    ]);
+
+    // sync first two permissions to role1
+    $role1->syncPermissions($permissions->take(2));
+
+    // sync last 2 permission to role2
+    $role2->syncPermissions($permissions->slice(1));
+
+    $user->assignRole([$role1, $role2]);
+
+    $role_permission_object_service = mock(RolePermissionObjectService::class, function (\Mockery\MockInterface $mock) use ($role2, $role1, $permissions) {
+
+        $result1 = collect([
+            $permissions[0]->name => [1,2,3],
+            $permissions[1]->name => [4,5,6],
+        ]);
+
+        $result2 = collect([
+            $permissions[1]->name => [7,8,9],
+            $permissions[2]->name => [10,11,12],
+        ]);
+
+        $mock->shouldReceive('get')
+            //->with($role1)
+            ->andReturn($result1, $result2);
+    });
+
+    // Act
+    $user_permission_service = new UserPermissionService($role_permission_object_service);
+
+    $result = $user_permission_service->get($user);
+
+    // Assert
+    expect($result['permissions'])
+        ->toHaveCount(3)
+        ->toHaveKeys($permissions->pluck('name')->toArray())
+        ->and($result['permissions'][$permissions[0]->name])->toBe([1,2,3])
+        ->and($result['permissions'][$permissions[1]->name])->toContain(4,5,6,7,8,9)
+        ->and($result['permissions'][$permissions[2]->name])->toBe([10,11,12]);
+});
