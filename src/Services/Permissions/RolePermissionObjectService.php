781cfa67c2789dc658beeca7452cccc688ed88d7
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-07-31 17:13:42 +0200

    feat(auth): add RolePermissionObjectService and test
    - Added RolePermissionObjectService class to handle role permissions
    - Created RolePermissionObjectServiceTest to test the functionality
    - Mocked RoleAffiliatedIdsService for testing purposes

---
+++ src/Services/Permissions/RolePermissionObjectService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Seatplus\Auth\Services\Permissions;
+
+use Illuminate\Support\Collection;
+use Seatplus\Auth\Models\Permissions\Permission;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Services\Roles\RoleAffiliatedIdsService;
+
+class RolePermissionObjectService
+{
+    public function __construct(
+        private ?RoleAffiliatedIdsService $role_affiliated_ids_service = null
+    )
+    {
+        $this->role_affiliated_ids_service = $role_affiliated_ids_service ?? new RoleAffiliatedIdsService();
+    }
+
+    public function get(Role $role): Collection
+    {
+        $role = $role->loadMissing('permissions');
+
+        $affiliated_ids = $this->role_affiliated_ids_service->get($role);
+
+        return $role->permissions
+            ->mapWithKeys(fn (Permission $permission) => [$permission->name => $affiliated_ids]);
+    }
+
+}
---
+++ tests/Unit/Services/Roles/RolePermissionObjectServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Str;
+use Seatplus\Auth\Models\Permissions\Permission;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Services\Permissions\RolePermissionObjectService;
+use Seatplus\Auth\Services\Roles\RoleAffiliatedIdsService;
+
+test('role permission object service', function () {
+    // Arrange
+    $role = Role::create(['name' => Str::random()]);
+
+    // create 3 permissions
+    $permissions = [
+        Permission::create(['name' => Str::random()]),
+        Permission::create(['name' => Str::random()]),
+        Permission::create(['name' => Str::random()]),
+    ];
+
+    $role->syncPermissions($permissions);
+
+    $mock = mock(RoleAffiliatedIdsService::class, function ($mock) {
+        $mock->shouldReceive('get')->andReturn([1,2,3]);
+    });
+
+    // Act
+    $role_permission_object_service = new RolePermissionObjectService($mock);
+
+    $result = $role_permission_object_service->get($role);
+
+    // Assert
+    expect($result)->toHaveCount(3)
+        ->toHaveKeys([$permissions[0]->name, $permissions[1]->name, $permissions[2]->name])
+        ->and($result[$permissions[0]->name])->toBe([1,2,3])
+        ->and($result[$permissions[1]->name])->toBe([1,2,3])
+        ->and($result[$permissions[2]->name])->toBe([1,2,3]);
+});
