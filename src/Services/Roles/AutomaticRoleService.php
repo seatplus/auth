ef39e1424c2c5684625679f74927bafce86eb669
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-08-02 16:00:10 +0200

    feat: Add AutomaticRoleServiceTest and AutomaticRoleService classes and methods
    Added the AutomaticRoleServiceTest.php file to test the AutomaticRoleService class and its methods for assigning roles to corporations, alliances, and users. Also added the AutomaticRoleService.php file with methods for automatically assigning roles to corporations and alliances, syncing members, and handling users.

---
+++ src/Services/Roles/AutomaticRoleService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Seatplus\Auth\Services\Roles;
+
+use Seatplus\Auth\Enums\RoleMembershipStatus;
+use Seatplus\Auth\Enums\RoleType;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Models\User;
+use Seatplus\Eveapi\Models\Alliance\AllianceInfo;
+use Seatplus\Eveapi\Models\Corporation\CorporationInfo;
+
+class AutomaticRoleService extends BaseRoleService implements RoleServiceInterface
+{
+
+    public function __construct(?Role $role = null)
+    {
+        if ($role) {
+            $this->for($role);
+        }
+    }
+
+    private function automaticallyAssignRoleToCorporation(int|string $corporation_id): void
+    {
+        $this->setRoleMembership($corporation_id, CorporationInfo::class);
+    }
+
+    private function automaticallyAssignRoleToAlliance(int|string $alliance_id): void
+    {
+        $this->setRoleMembership($alliance_id, AllianceInfo::class);
+    }
+
+    public function automaticallyAssignRoleTo(?array $corporation_ids = [], ?array $alliance_ids = []): void
+    {
+        $this->setRoleType(RoleType::AUTOMATIC);
+
+        // reset all role memberships
+        $this->resetRoleMembership();
+
+        // for each corporation_id, we assign the role to the corporation
+        foreach ($corporation_ids as $corporation_id) {
+            $this->automaticallyAssignRoleToCorporation($corporation_id);
+        }
+
+        foreach ($alliance_ids as $alliance_id) {
+            $this->automaticallyAssignRoleToAlliance($alliance_id);
+        }
+
+        $this->syncMembers();
+        $this->handleUsers();
+    }
+
+    public function syncMembers(): void
+    {
+        $character_ids = $this->getAssignedCharacterIds();
+
+        // since this is an automatic role, we directly want to assign users that have a character with the required corporation_id or alliance_id
+        $users = $this->getUsersFromCharacterIds($character_ids);
+
+        // remove members that are not within users
+        $this->removeIneligibleMembers($users->pluck('id')->all());
+
+        // add members that are not in role membership
+        $users->each(fn($user) => $this->setRoleMembership(
+            entity_id: $user->id,
+            entity_type: User::class,
+            status: $this->isUserCompliant($user) ? RoleMembershipStatus::ACTIVE : RoleMembershipStatus::INACTIVE
+        ));
+    }
+}
---
+++ tests/Unit/Services/Roles/AutomaticRoleServiceTest.php
@@ -0,0 +1,100 @@
+<?php
+
+use Seatplus\Auth\Models\AccessControl\RoleMembership;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Models\User;
+use Seatplus\Auth\Services\Roles\AutomaticRoleService;
+
+beforeEach(function () {
+    $this->role = Role::create(['name' => 'test']);
+    $this->role = $this->role->refresh();
+    $this->service = new AutomaticRoleService();
+
+    $this->service->for($this->role);
+});
+
+describe('assigning', function () {
+    it('role to corporation and getting role on test user', function () {
+
+        $test_character = test()->test_character;
+        $corporation_id = $test_character->corporation_id;
+
+        expect(test()->test_user->refresh()->hasRole($this->role->name))->toBeFalse();
+
+        $this->service->automaticallyAssignRoleTo(corporation_ids: [$corporation_id]);
+
+        expect(RoleMembership::get())->toHaveCount(2) // User and Character
+            ->and($this->service->getAssignedCharacterIds())->toContain($test_character->character_id)
+            ->and(test()->test_user->refresh()->hasRole($this->role->name))->toBeTrue();
+
+    });
+
+    it('role to alliance', function () {
+
+        $test_character = test()->test_character;
+        $alliance_id = $test_character->alliance_id;
+
+        $this->service->automaticallyAssignRoleTo(alliance_ids: [$alliance_id]);
+
+        expect($this->service->getAssignedCharacterIds())->toContain($test_character->character_id);
+    });
+
+    it('role to corporation and alliance', function () {
+
+        $test_character = test()->test_character;
+        $corporation_id = $test_character->corporation_id;
+        $alliance_id = $test_character->alliance_id;
+
+        $this->service->automaticallyAssignRoleTo(corporation_ids: [$corporation_id], alliance_ids: [$alliance_id]);
+
+        expect($this->service->getAssignedCharacterIds())->toContain($test_character->character_id);
+    });
+});
+
+describe('handling Members', function () {
+    it('removes role from user if nothing is assigned', function () {
+
+        $test_user = test()->test_user;
+
+        $test_user->assignRole($this->role);
+
+        expect(test()->test_user->refresh()->hasRole($this->role->name))->toBeTrue();
+
+        $this->service->automaticallyAssignRoleTo();
+
+        expect(test()->test_user->refresh()->hasRole($this->role->name))->toBeFalse()
+            ->and(RoleMembership::query()->count())->toBe(0);
+    });
+
+    it('works also with role in constructor', function () {
+        /** @var Role $role */
+        $role = Role::create(['name' => 'constructor test']);
+
+        $service = new AutomaticRoleService($role);
+
+        $test_character = test()->test_character;
+        $corporation_id = $test_character->corporation_id;
+
+        $service->automaticallyAssignRoleTo(corporation_ids: [$corporation_id]);
+
+        expect(RoleMembership::get())->toHaveCount(2) // User and Character
+        ->and($service->getAssignedCharacterIds())->toContain($test_character->character_id)
+            ->and(test()->test_user->refresh()->hasRole($role->name))->toBeTrue();
+    });
+});
+
+it('sets role type to automatic', function () {
+
+    expect($this->role->type)->toBe('manual');
+
+    $this->service->automaticallyAssignRoleTo();
+
+    expect($this->role->type)->toBe('automatic');
+});
+
+
+
+
+
+
+
