2c18b3898b62bc3648baeaaf1df321040ed79907
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-08-09 14:13:38 +0200

    feat(auth): add ManualRoleService and ManualRoleServiceTest
    - Added ManualRoleService class with methods to set moderator, add member, remove member, and sync members
    - Added corresponding tests for ManualRoleService functionality

---
+++ src/Services/Roles/ManualRoleService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Seatplus\Auth\Services\Roles;
+
+use Seatplus\Auth\Enums\RoleMembershipStatus;
+use Seatplus\Auth\Models\User;
+
+class ManualRoleService extends AbstractRoleService implements RoleServiceInterface
+{
+    public function setModerator(User $user, bool $can_moderate = true): void
+    {
+        $this->setRoleMembership(
+            entity_id: $user->id,
+            entity_type: User::class,
+            can_moderate: $can_moderate
+        );
+    }
+
+    public function addMember(User $user): void
+    {
+        $this->setRoleMembership(
+            entity_id: $user->id,
+            entity_type: User::class,
+            status: RoleMembershipStatus::ACTIVE
+        );
+    }
+
+    public function removeMember(User $user): void
+    {
+        $this->removeRoleMembership($user);
+    }
+
+    public function syncMembers(): void
+    {
+        // update the status of the members based on the user compliance
+        $this->updateMemberStatusBasedOnUserCompliance();
+    }
+}
---
+++ tests/Unit/Services/Roles/ManualRoleServiceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use Seatplus\Auth\Enums\RoleMembershipStatus;
+use Seatplus\Auth\Models\AccessControl\RoleMembership;
+use Seatplus\Auth\Models\Permissions\Role;
+use Seatplus\Auth\Models\User;
+use Seatplus\Auth\Services\Roles\ManualRoleService;
+
+beforeEach(function () {
+    $this->role = Role::create(['name' => 'test']);
+    $this->role = $this->role->refresh();
+
+    $this->service = new ManualRoleService($this->role);
+});
+
+it('can add a member', function () {
+    $test_user = test()->test_user;
+
+    $this->service->addMember($test_user);
+
+    expect(RoleMembership::query()->count())->toBe(1)
+        ->and(RoleMembership::first())->entity_type->toBe(User::class);
+});
+
+it('can remove a member', function () {
+    $test_user = test()->test_user;
+
+    $this->service->addMember($test_user);
+
+    expect(RoleMembership::query()->count())->toBe(1);
+
+    $this->service->removeMember($test_user);
+
+    expect(RoleMembership::query()->count())->toBe(0);
+});
+
+it('can add user as moderator and does not change status', function () {
+    $test_user = test()->test_user;
+
+    $this->service->addMember($test_user);
+
+    expect(RoleMembership::first())->status->toBe(RoleMembershipStatus::ACTIVE->value);
+
+    $this->service->setModerator($test_user);
+
+    expect(RoleMembership::first())->status->toBe(RoleMembershipStatus::ACTIVE->value)
+        ->can_moderate->toBeTrue();
+
+    $this->service->setModerator($test_user, false);
+
+    expect(RoleMembership::first())->status->toBe(RoleMembershipStatus::ACTIVE->value)
+        ->can_moderate->toBeFalse();
+});
+
+it('syncs members', function () {
+    $test_user = test()->test_user;
+
+    $this->service->addMember($test_user);
+
+    expect(RoleMembership::first())->status->toBe(RoleMembershipStatus::ACTIVE->value);
+
+    $this->service->syncMembers();
+
+    expect(RoleMembership::first())->status->toBe(RoleMembershipStatus::ACTIVE->value);
+});
