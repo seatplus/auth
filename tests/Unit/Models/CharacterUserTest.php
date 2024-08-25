d907c5aef8e37e50cd0d495cbd94af258f669cad
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-08-25 22:46:58 +0200

    Add CharacterUserTest.php

---
+++ tests/Unit/Models/CharacterUserTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('has character', function () {
+    $test_user = $this->test_user;
+
+    $character_user = $test_user->character_users->first();
+
+    expect($character_user->character->character_id)->toBe($this->test_character->character_id);
+});
