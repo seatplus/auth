b68f6f6a8ed07db023bdff68d6609c09362e294c
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-09-09 22:41:30 +0200

    Add ManageOptInRoleAction and ManageOptInRoleActionTest files.

---
+++ src/Http/Actions/Roles/OptIn/ManageOptInRoleAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Seatplus\Auth\Http\Actions\Roles\OptIn;
+
+use Illuminate\Support\Arr;
+use Seatplus\Auth\Enums\RoleType;
+use Seatplus\Auth\Http\Requests\RoleRequest;
+use Seatplus\Auth\Services\Roles\BaseRoleService;
+use Seatplus\Auth\Services\Roles\OnRequestRoleService;
+
+class ManageOptInRoleAction
+{
+    public function __construct(
+        protected BaseRoleService $baseRoleService
+    ) {
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function execute(RoleRequest $request): void
+    {
+        $this->checkPermission();
+
+        $validated = $request->validated();
+
+        $roleService = $this->baseRoleService->for($validated['role_id'])->optIn();
+
+        if ($name = Arr::get($validated, 'name')) {
+            $roleService->updateRoleName($name);
+        }
+
+        if ($affiliated = Arr::get($validated, 'affiliated')) {
+            $roleService->syncAffiliateManyEntities($affiliated);
+        }
+
+        if ($assigned = Arr::get($validated, 'assigned')) {
+            $roleService->addCriteriaForRole($assigned);
+        }
+
+        $roleService->setRoleType(RoleType::OPT_IN);
+    }
+
+    private function checkPermission(): void
+    {
+        if (! auth()->user()->can('administrate access control groups')) {
+            abort(403, 'You are not allowed to administrate access control groups');
+        }
+    }
+
+}
